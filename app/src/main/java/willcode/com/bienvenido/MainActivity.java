c1ae6a43ef6626564aed41b38d0fa715bda872ea
GeoWilly <william.lga012@gmail.com>
2020-09-21 12:07:58 -0300

    Initial commit

---
+++ app/src/main/java/willcode/com/bienvenido/MainActivity.java
@@ -0,0 +1,20 @@
+package willcode.com.bienvenido;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
